a5b35bdc99eac5d9329691d387dea34b45c6202d
Toni  Diep <adiep123@gmail.com>
2020-04-03 19:02:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet(planeteer_calls) 
+def summon_captain_planet(planeteer_calls)
 
 
     {planeteer_calls.map(&:upcase)} do |planeteer_calls|
