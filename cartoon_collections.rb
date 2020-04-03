15e09e8d90e4461758b790dcb0ff041132c79458
Toni  Diep <adiep123@gmail.com>
2020-04-03 19:38:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_the_cheese(cheese_potentials)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_potentials.find do |cheese_types|
-  cheese_types.include?(cheese_types)
+  cheese_potentials.find do |cheese_type|
+  cheese_types.include?(cheese_type)
   end
 end
