17ff9706ee22047c7378f921fa963eed99874b8e
Toni  Diep <adiep123@gmail.com>
2020-04-03 19:26:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,7 @@ def long_planeteer_calls(array)# code an argument here
   else return false
 end
 end
+
 def find_the_cheese(cheese_potentials)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
