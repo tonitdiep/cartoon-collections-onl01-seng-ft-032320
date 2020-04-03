957ca5bfb9a8af403ef392d532d56f84557cf98f
Toni  Diep <adiep123@gmail.com>
2020-04-03 14:37:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,16 @@
 def roll_call_dwarves(dwarves_list) # code an argument here
-  dwarves_list.each |dwarf| #_with_index
+  #return_dwarves_list = []
+  dwarves_list.each_with_index |dwarf|
   #dwarves_list << return_dwarves_list {"#{index+1}" "#{name}"}
-
+#end
+  #puts dwarves_list
+   #return_dwarves_list
 end
 
 def summon_captain_planet(planeteer_calls) #upcase/element & w/ "!", twice again
-  #  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  #  planeteer_calls.index[i]
-  #  planeteer_calls.map(&:upcase!) #do |i|
+    planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    planeteer_calls.index[i]
+    planeteer_calls.map(&:upcase!) #do |i|
 end
 
 def long_planeteer_calls()# code an argument here
@@ -17,5 +20,5 @@ end
 def find_the_cheese(cheese_potentials)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
+  
 end
