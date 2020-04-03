ad61e64aa6edc7ae055cba38d6a497668b87f2ca
Toni  Diep <adiep123@gmail.com>
2020-04-03 18:54:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do {|name, index|}
     index += 1
-    puts |name, index|
+    puts "#{index}. #{name}"
   end
 def summon_captain_planet(planeteer_calls) #upcase/element & w/ "!", twice again
     #planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
