4c9781c714d8200e771ff1b0b8a4f27e1d2d5bb4
Toni  Diep <adiep123@gmail.com>
2020-04-03 18:23:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index
-    puts dwarves.count
+    puts dwarves.sort
   end
 def summon_captain_planet(planeteer_calls) #upcase/element & w/ "!", twice again
     #planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
