c075258365cb6a05e1eedf1bcf37d2790ceaef7f
Toni  Diep <adiep123@gmail.com>
2020-04-03 17:44:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index
-  dwarves << {|dwarf, index|}
   end
 end
 def summon_captain_planet(planeteer_calls) #upcase/element & w/ "!", twice again
