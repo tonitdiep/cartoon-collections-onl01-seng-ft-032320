94f6cbd8de6d8257123735162940a77ba218bd23
Toni  Diep <adiep123@gmail.com>
2020-04-03 18:58:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,10 +4,11 @@ def roll_call_dwarves(dwarves)
     puts "#{index}. #{name}"
   end
 end
-def summon_captain_planet(planeteer_calls) #upcase/element & w/ "!", twice again
+
+def summon_captain_planet(planeteer_calls) 
     #planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
     #planeteer_calls.index[i]
-    #planeteer_calls.map(&:upcase!) do |planeteer_calls|
+    planeteer_calls.map(&:upcase!) do |planeteer_calls|
 end
 
 def long_planeteer_calls()# code an argument here
