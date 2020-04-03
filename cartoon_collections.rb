9fcf2c71e1cf96f3b54dc91dccdbc3d2c3f76dc5
Toni  Diep <adiep123@gmail.com>
2020-04-03 19:31:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,16 +11,16 @@ def summon_captain_planet(planeteer_calls)
     end
 end
 
-def long_planeteer_calls(array)# code an argument here
-    i = 0
+def long_planeteer_calls(array)
     if array.any? {|i| i.length > 4}
     true
     else return false
     end
 end
 
-def find_the_cheese(cheese_potentials)# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_potentials)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese_types|
+    
 
 end
